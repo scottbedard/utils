90db8028aee8d7b2b494ae37ef67466ecc19d30b
Scott Bedard <scottbedard90@gmail.com>
2021-08-27 21:24:09 -0500

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+  },
+}
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'rollup'
 import { terser } from 'rollup-plugin-terser'
 import del from 'rollup-plugin-delete'
+import eslint from '@rollup/plugin-eslint'
 import pkg from './package.json'
 import ts from 'rollup-plugin-ts'
 
@@ -28,6 +29,7 @@ export default defineConfig({
   ],
   plugins: [
     del({ targets: 'dist/*' }),
+    eslint({ fix: true }),
     ts(),
   ],
 })
