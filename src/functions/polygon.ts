90383564d03b7c1f9433c2fbde3824256440d5c1
Scott Bedard <scottbedard90@gmail.com>
2022-07-31 12:24:09 -0500

    update polygon description

--- src/functions/polygon.ts
+++ src/functions/polygon.ts
@@ -2,7 +2,8 @@ import { rotateVector } from './rotateVector'
 import { Vector2 } from '../utils'
 
 /**
- * Create a regular polygon.
+ * Create a regular polygon starting at the 12 o'clock position,
+ * and rotating counter-clockwise around the origin
  */
 export function polygon(sides: number, circumradius = 1): Vector2[] {
   return new Array(sides).fill(null).map((n, i) => {
