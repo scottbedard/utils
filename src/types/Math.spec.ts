d831ce5096f0c74dccd54b254eda2aaece21615f
Scott Bedard <scottbedard90@gmail.com>
2023-06-28 11:57:39 -0500

    fix assertion typo

--- src/types/Math.spec.ts
+++ src/types/Math.spec.ts
@@ -66,8 +66,8 @@ describe('math', () => {
     it('XOR<T, U>', () => {
       type T00 = Expect<Equal<XOR<0, 0>, 0>>
       type T01 = Expect<Equal<XOR<0, 1>, 1>>
-      type T10 = Expect<Equal<XOR<0, 0>, 0>>
-      type T11 = Expect<Equal<XOR<0, 0>, 0>>
+      type T10 = Expect<Equal<XOR<1, 0>, 1>>
+      type T11 = Expect<Equal<XOR<1, 1>, 0>>
     })
 
     it('XNOR<T, U>', () => {
