071a1621b574cf0b1cf058f73bcb5a9cabc597a7
Scott Bedard <scottbedard90@gmail.com>
2024-08-26 20:06:16 -0500

    increase timeout

--- src/functions/timeout.spec.ts
+++ src/functions/timeout.spec.ts
@@ -4,8 +4,8 @@ describe('timeout', () => {
   it('should wait for the specified amount of time to resolve', async () => {
     const start = Date.now()
 
-    await timeout(100)
+    await timeout(500)
     
-    expect(Date.now() - start).toBeGreaterThanOrEqual(100)
+    expect(Date.now() - start).toBeGreaterThanOrEqual(500)
   })
 })
